3a8e63af5a90ddc81381e73b509861bfb919e080
William Chui <williamchui@supermicro.com>
2022-02-01 12:53:00 -0800

    Update README.md. Fix with missing pagination number.

--- example/src/App.js
+++ example/src/App.js
@@ -4,11 +4,11 @@ import '@willchui/my-bootstrap-table/dist/index.css';
 import tabledata from './sample-data.json';
 
 const App = () => {
-  const header = {'userId': {'title': 'User ID', 'width': '80px'},
+  const header = {'userId': {'title': 'User ID', 'width': '60px'},
                   'id': {'title':'ID', 'width': '60px'},
                   'title': {'title':'Description'}, 
                   'completed': {'title':'Is Complete', 'width': '200px'}};
-  return <MyBootstrapTable className="m-3"  header={header} tabledata={tabledata} pagesize={20} ></MyBootstrapTable>
+  return <MyBootstrapTable className="m-3" header={header} tabledata={tabledata} paginationsize={5} pagesize={20} ></MyBootstrapTable>
 }
 
 export default App
